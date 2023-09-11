f66709c540d3f077a345ca9f2c69186423643f6a
htetaungkhant <htetaung23.ha@gmail.com>
2023-09-11 20:45:35 +0630

    updated: upgrade version by using interface and generic to add2Promise

--- lessons/add2Promises.ts
+++ lessons/add2Promises.ts
@@ -1,20 +1,25 @@
 //Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two numbers.
 
-async function addTwoPromises(promise1: Promise<number>, promise2: Promise<number>): Promise<number> {
-  return promise1.then(async value => {
-    return promise2.then(res => {
-      return res + value;
+interface IGeneric {
+  value: number;
+}
+
+async function addTwoPromises<T extends IGeneric>(promise1: Promise<T>, promise2: Promise<T>): Promise<T> {
+  return promise1.then(async value1 => {
+    return promise2.then(async value2 => {
+      return { value: value1.value + value2.value } as T;
     });
   });
 }
 
-const promise1 = new Promise<number>(resolve => setTimeout(() => resolve(2), 20));
-const promise2 = new Promise<number>(resolve => setTimeout(() => resolve(5), 60));
+const promise1 = new Promise<IGeneric>(resolve => setTimeout(() => resolve({ value: 2 }), 20));
+const promise2 = new Promise<IGeneric>(resolve => setTimeout(() => resolve({ value: 5 }), 60));
 
-const promise3 = new Promise<number>(resolve => setTimeout(() => resolve(10), 50));
-const promise4 = new Promise<number>(resolve => setTimeout(() => resolve(-12), 30));
+const promise3 = new Promise<IGeneric>(resolve => setTimeout(() => resolve({ value: 10 }), 50));
+const promise4 = new Promise<IGeneric>(resolve => setTimeout(() => resolve({ value: -12 }), 30));
 
 const result = addTwoPromises(promise1, promise2);
 const result2 = addTwoPromises(promise3, promise4);
+
 result.then(val => console.log(val));
 result2.then(val => console.log(val));
