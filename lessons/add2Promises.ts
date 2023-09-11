dcf074e62b73d5d4ea6222c9c7a967c0fc060f48
htetaungkhant <htetaung23.ha@gmail.com>
2023-09-11 20:18:55 +0630

    v1: first version of add2Promise

--- lessons/add2Promises.ts
+++ lessons/add2Promises.ts
@@ -1,33 +1,25 @@
 //Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two numbers.
 
-// async function addTwoPromises<T>(promise1: Promise<T>, promise2: Promise<T>): Promise<T> {
+async function addTwoPromises(promise1: Promise<number>, promise2: Promise<number>): Promise<number> {
+  let total: number = 0;
+  return promise1
+    .then(value => {
+      total = value;
+      return promise2;
+    })
+    .then(res => {
+      total += res;
+      return total;
+    });
+}
 
-// }
-// const promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20));
+const promise1 = new Promise<number>(resolve => setTimeout(() => resolve(2), 20));
+const promise2 = new Promise<number>(resolve => setTimeout(() => resolve(5), 60));
 
-// const value = promise1.then(async value => value).catch(err => console.log(err));
+const promise3 = new Promise<number>(resolve => setTimeout(() => resolve(10), 50));
+const promise4 = new Promise<number>(resolve => setTimeout(() => resolve(-12), 30));
 
-// console.log(value);
-
-// const promise2 = new Promise(resolve => setTimeout(() => resolve(5), 60));
-// console.log(promise2);
-
-const myFun = async () => {
-  const myPromise = await new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("foo");
-    }, 300);
-  });
-  return myPromise;
-};
-
-let str;
-
-str = myFun()
-  .then(value => {
-    console.log(value, "no way");
-  })
-  .then(res => res)
-  .catch(err => console.log(err));
-
-console.log(str);
+const result = addTwoPromises(promise1, promise2);
+const result2 = addTwoPromises(promise3, promise4);
+result.then(val => console.log(val));
+result2.then(val => console.log(val));
