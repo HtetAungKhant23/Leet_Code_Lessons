a085feb9abdc2af8c6edb1705ea41dd78a90401f
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-01-02 23:22:23 +0630

    add: clone a new graph using dfs and recursive

--- lessons/cloneGraph.ts
+++ lessons/cloneGraph.ts
@@ -1,9 +1,46 @@
 // learn and implement with ts
 class Vertex {
   val: number;
-  neighbors: number[];
-  constructor(val: number, neighbors: number[]) {
+  neighbors: Vertex[];
+  constructor(val?: number, neighbors?: Vertex[]) {
     this.val = val || 0;
     this.neighbors = neighbors || [];
+  }
+}
+
+class Solution {
+  dfs(cur: Vertex | null, mp: Map<Vertex, Vertex>): Vertex | null {
+    if (cur === null) {
+      return null;
+    }
+
+    const neighbour: Vertex[] = [];
+    const clone: Vertex = new Vertex(cur.val);
+    mp.set(cur, clone);
+
+    for (const it of cur.neighbors) {
+      if (mp.has(it)) {
+        neighbour.push(mp.get(it)!);
+      } else {
+        neighbour.push(this.dfs(it, mp)!);
+      }
+    }
+
+    clone.neighbors = neighbour;
+    return clone;
+  }
+
+  cloneGraph(node: Vertex | null): Vertex | null {
+    const mp: Map<Vertex, Vertex> = new Map();
+
+    if (node === null) {
+      return null;
+    }
+
+    if (node.neighbors.length === 0) {
+      return new Vertex(node.val);
+    }
+
+    return this.dfs(node, mp);
   }
 }
