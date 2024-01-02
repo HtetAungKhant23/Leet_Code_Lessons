06f34f397ddee1d47861ee7d69354f5af470cb5d
Htet Aung Khant <htetaung23.ha@gmail.com>
2024-01-02 23:14:48 +0630

    add: clone a new graph using dfs and recursive

--- lessons/cloneGraph.cpp
+++ lessons/cloneGraph.cpp
@@ -21,3 +21,44 @@ public:
         neighbors = _neighbors;
     }
 };
+
+class Solution {
+public:
+    // DFS function to clone the graph
+    Node* dfs(Node* cur, unordered_map<Node*, Node*>& mp) {
+        vector<Node*> neighbour;
+        Node* clone = new Node(cur->val);  // Create a new node with the current node's value
+        mp[cur] = clone;  // Store the mapping between original and cloned nodes in the map
+
+        // Iterate through the neighbors of the current node
+        for (auto it : cur->neighbors) {
+            if (mp.find(it) != mp.end()) {
+                // If the neighbor is already cloned, retrieve it from the map
+                neighbour.push_back(mp[it]);
+            } else {
+                // If the neighbor is not cloned yet, recursively clone it
+                neighbour.push_back(dfs(it, mp));
+            }
+        }
+
+        clone->neighbors = neighbour;  // Set the cloned node's neighbors to the cloned neighbors
+        return clone;
+    }
+
+    // Main function to clone the entire graph
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node*, Node*> mp;  // Map to store the mapping between original and cloned nodes
+
+        if (node == NULL) {
+            return NULL;
+        }
+
+        if (node->neighbors.size() == 0) {
+            // If only one node is present and has no neighbors
+            Node* clone = new Node(node->val);
+            return clone;
+        }
+
+        return dfs(node, mp);  // Perform DFS to clone the graph
+    }
+};
